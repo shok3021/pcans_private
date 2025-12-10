a3f7ed5d2c51db82e562115a749e411c2511c1ee
shok <shohgo.okazaki2@gmail.com>
2025-12-10 18:04:04 +0900

    changed walltime to 1h to test

--- em2d_mpi/md_mrx/run_mrx.sh
+++ em2d_mpi/md_mrx/run_mrx.sh
@@ -2,7 +2,7 @@
 #PBS -q A_S
 #PBS -P NIFS25KIST067
 #PBS -l select=1:ncpus=160:mpiprocs=160:ompthreads=1:mem=400gb
-#PBS -l walltime=12:00:00
+#PBS -l walltime=01:00:00
 
 module load intel/2025.1
 module load openmpi/5.0.7/gcc11.5.0
