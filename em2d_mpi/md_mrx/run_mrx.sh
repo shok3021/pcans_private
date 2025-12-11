bd06ff2964bc494a69bac19d1377dde1446bb311
shok <shohgo.okazaki2@gmail.com>
2025-12-11 14:27:14 +0900

    元に戻した

--- em2d_mpi/md_mrx/run_mrx.sh
+++ em2d_mpi/md_mrx/run_mrx.sh
@@ -4,23 +4,14 @@
 #PBS -l select=1:ncpus=160:mpiprocs=160:ompthreads=1:mem=400gb
 #PBS -l walltime=01:00:00
 
-# 環境をクリーンにして OpenMPI をロード
-module purge
+module load intel/2025.1
 module load openmpi/5.0.7/gcc11.5.0
 
 cd ${PBS_O_WORKDIR}
 
-# スタックサイズ制限を解除（念のため）
-ulimit -s unlimited
-
 export FC=$(which mpif90)
-
-# 確認用ログ
-echo "Current FC: $FC"
-echo "Current Directory: $(pwd)"
 
 make clean
 make
 
-# 実行
 mpirun ./a.out
