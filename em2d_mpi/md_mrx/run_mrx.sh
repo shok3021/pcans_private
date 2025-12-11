6329b44f5d84ef1eb7c1d6a08962b43f93753e25
shok <shohgo.okazaki2@gmail.com>
2025-12-11 14:01:41 +0900

    changed module load

--- em2d_mpi/md_mrx/run_mrx.sh
+++ em2d_mpi/md_mrx/run_mrx.sh
@@ -4,7 +4,7 @@
 #PBS -l select=1:ncpus=160:mpiprocs=160:ompthreads=1:mem=400gb
 #PBS -l walltime=01:00:00
 
-module load intel/2025.1
+# module load intel/2025.1
 module load openmpi/5.0.7/gcc11.5.0
 
 cd ${PBS_O_WORKDIR}
