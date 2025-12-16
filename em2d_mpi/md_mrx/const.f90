440f74467e3379f30d38c8a8964c6183a6d86dc2
shok <shohgo.okazaki2@gmail.com>
2025-12-16 18:47:36 +0900

    戻した

--- em2d_mpi/md_mrx/const.f90
+++ em2d_mpi/md_mrx/const.f90
@@ -16,7 +16,7 @@ module const
   integer, parameter :: nproc = 160 ! for thin harris sheet !default: 12       ! NUMBER OF PROCESSORS
 
 !! SETUP FOR SUBROUTINES CALLED IN MAIN PROGRAM
-  integer, parameter :: itmax  = 1001 ! 50000	!NUMBER OF ITERATION
+  integer, parameter :: itmax  = 50000	!NUMBER OF ITERATION
   integer            :: it0    = 0	!0:INITIAL, NONZERO/9999999: RESTART DATA
   integer, parameter :: intvl1 = 500	!INTERVAL FOR PARTICLES & FIELDS STORAGE
   integer, parameter :: intvl2 = 50	!INTERVAL FOR ENERGY CALC.
